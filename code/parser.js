cf3bb2cebb9a6e14428a8331666589e6b0218235
nikola-42c <nikola.vukic@42computing.tech>
2024-10-11 01:47:08 +0200

    commented parser

--- code/parser.js
+++ code/parser.js
@@ -14,7 +14,7 @@ const parseContracts = (contracts) => {
 
 const parseLoops = (loopTypes, statements, maxNested) => {
   let currentNesting = 0;
-  let maxCurrentNesting = 0;
+  let maxNestedInCurrent = 0;
 
   for (const statement of statements) {
     if (loopTypes.has(statement.type)) {
@@ -26,17 +26,23 @@ const parseLoops = (loopTypes, statements, maxNested) => {
         maxNested
       );
 
-      maxCurrentNesting = Math.max(
+      // since curerntNesting has to be reset,
+      // in order to return the amount of nesting found
+      // we need to keep track of it
+      maxNestedInCurrent = Math.max(
         currentNesting + bodyNested,
-        maxCurrentNesting
+        maxNestedInCurrent
       );
 
+      // cannot return this as then same loop will get counter multiple times
       maxNested.value = Math.max(maxNested.value, currentNesting + bodyNested);
     }
+    // needs to reset after every loop so loops
+    // do not get added together
     currentNesting = 0;
   }
 
-  return maxCurrentNesting;
+  return maxNestedInCurrent;
 };
 
 module.exports = {
