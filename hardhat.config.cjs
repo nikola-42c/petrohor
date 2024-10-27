0647c4f92b49fe4626c89c4148e596702bcfbf05
nikola-42c <nikola.vukic@42computing.tech>
2024-10-27 19:16:00 +0100

    gitignore update

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
     hardhat: {
       forking: {
         url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+        loggingEnabled: true,
       },
     },
   },
